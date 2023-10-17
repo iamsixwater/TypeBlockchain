10dd54e33a1bf0eb0ac4643b769188a28ad2b7bc
iamsixwater <soup.sixwater@gmail.com>
2023-10-17 14:18:35 +1100

    add simple blockchain concept

---
+++ build/index.js
@@ -0,0 +1,35 @@
+import * as crypto from 'crypto';
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash('sha256').update(toHash).digest('hex');
+    }
+}
+class Blockchain {
+    constructor() {
+        this.blocks = [];
+    }
+    addBlock(data) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    getBlocks() {
+        return [...this.blocks];
+    }
+    getPrevHash() {
+        if (this.blocks.length < 1)
+            return '';
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+}
+const blockchain = new Blockchain();
+blockchain.addBlock('first one');
+blockchain.addBlock('second one');
+blockchain.addBlock('third one');
+console.log(blockchain.getBlocks());
